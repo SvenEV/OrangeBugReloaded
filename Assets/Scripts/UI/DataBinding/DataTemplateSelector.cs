127f02f2734c3fecf82d59769e23e232082912a9
SvenEV <sven@vinkemeier.de>
2016-03-02 01:15:35 +0100

    Clean-up: Removed legacy Unity projects and files.

--- Assets/Scripts/UI/DataBinding/DataTemplateSelector.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace SvenVinkemeier.Unity.UI.DataBinding
-{
-    public abstract class DataTemplateSelector : MonoBehaviour
-    {
-        public abstract GameObject SelectTemplate(object value);
-    }
-}
